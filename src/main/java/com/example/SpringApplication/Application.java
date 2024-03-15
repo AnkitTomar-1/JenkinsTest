6edbcd9f110316c1a904d5b62b3949c8e47e6444
AnkitTomar-1 <ankittomar8287@gmail.com>
2024-03-15 09:53:48 +0530

    fdf

--- src/main/java/com/example/SpringApplication/Application.java
+++ src/main/java/com/example/SpringApplication/Application.java
@@ -2,9 +2,17 @@ package com.example.SpringApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
+
+	@GetMapping
+	public String message(){
+		return "welcome to javatechie";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
